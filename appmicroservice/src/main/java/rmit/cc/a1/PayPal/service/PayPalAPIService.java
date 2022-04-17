eee68db3830302784c6e97d74f8a1d06e09dcd3b
Jingxuan Feng <henryfeng24@pm.me>
2022-04-17 14:38:02 +1000

    Changed naming

---
+++ appmicroservice/src/main/java/rmit/cc/a1/PayPal/service/PayPalAPIService.java
@@ -0,0 +1,14 @@
+package rmit.cc.a1.PayPal.service;
+
+import org.springframework.stereotype.Service;
+
+import com.paypal.base.rest.APIContext;
+
+@Service
+public class PayPalAPIService {
+
+    private APIContext paypalAPI;
+
+
+
+}
---
+++ appmicroservice/src/main/java/rmit/cc/a1/Transaction/model/Transaction.java
@@ -0,0 +1,65 @@
+package rmit.cc.a1.Transaction.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@Table(name = "transaction")
+public class Transaction {
+
+    @Id
+    @Column(name = "TransactionID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long TransactionID;
+
+    @Column(name = "buyerID")
+    private Long buyerID;
+
+    @Column(name = "SellerID")
+    private Long sellerID;
+    @Column(name = "TimeOfPurchase")
+    private Date TimeOfPurchase;
+
+    @Column(name = "update_At")
+    private Date update_At;
+
+    @Column(name = "itemListing_id")
+    private Long itemListingID;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "Status")
+    private String status;
+
+    @Column(name = "TrackingNumber")
+    private String TrackingNumber;
+
+    @PrePersist
+    protected void onCreate() {
+        this.TimeOfPurchase = new Date();
+        this.status = "Processing";
+    }
+
+    public Transaction(Long buyerID, Long sellerID, Long itemListingID, Double price, String currency, String trackingNumber) {
+        this.buyerID = buyerID;
+        this.sellerID = sellerID;
+        this.itemListingID = itemListingID;
+        this.price = price;
+        this.currency = currency;
+        TrackingNumber = trackingNumber;
+    }
+
+}
