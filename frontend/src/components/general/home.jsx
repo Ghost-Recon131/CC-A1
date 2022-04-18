289b17cd94df84cef99b61654beb82dd9eda9828
Jingxuan Feng <henryfeng24@pm.me>
2022-04-18 22:02:30 +1000

    fixed home not displaying properly bug

--- frontend/src/components/general/home.jsx
+++ frontend/src/components/general/home.jsx
@@ -1,19 +1,38 @@
 import { getGlobalState, setGlobalState } from "utils/globalState";
 import cookie from "js-cookie";
-import { useEffect } from "react";
-import {
-  useNavigate,
-} from 'react-router-dom';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 
 export default function Component() {
-  const navigate = useNavigate();
+  var navigate = useNavigate();
+  // initial state
+  var [itemListings, setItemListings] = useState([]);
 
   useEffect(() => {
-    // check if logged in
-    // if (!cookie.get("jwt")) {
-    //   navigate('/signin')
-    // }
+    // Do axios call here
+    async function axiosPost() {
+      var res = await axios.get(
+        getGlobalState("backendDomain") + "/api/itemListings/viewAllListings"
+      );
+      console.log(JSON.stringify(res.data))
+      setItemListings(res.data);
+    }
+    axiosPost();
   }, []);
 
-  return <div>HELLO THERE</div>;
+  return (
+    <div>
+      {itemListings.map((itemListing) => (
+        <div key={itemListing.id}>
+          {/* TODO: Double check values here */}
+          <Link to={"/itemListing?id=" + itemListing.id}>
+            {itemListing.listingTitle}
+          </Link>
+          ${itemListing.price}
+        </div>
+      ))}
+      {/* <img src="https://cdn.vox-cdn.com/thumbor/Pkmq1nm3skO0-j693JTMd7RL0Zk=/0x0:2012x1341/1200x800/filters:focal(0x0:2012x1341)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg"></img> */}
+    </div>
+  );
 }
