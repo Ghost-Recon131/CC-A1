de361ac7bb2b5db0e1c5cd87211da049d6d13385
Jingxuan Feng <henryfeng24@pm.me>
2022-04-20 23:14:32 +1000

    bug fixes

--- frontend/src/components/itemlisting/itemlisting.jsx
+++ frontend/src/components/itemlisting/itemlisting.jsx
@@ -11,9 +11,9 @@ export default function Component() {
   var queryParams = new URLSearchParams(window.location.search);
   var id = queryParams.get("id");
   var [user, setUser] = useState({});
-  var [checkOutData, setCheckOutData] = useState({buyerID: "", sellerID: "", itemListingID: "", price: "", currency: "AUD"});
   var [confirmDelete, setConfirmDelete] = useState(false);
   const [confirmPurchase, setConfirmPurchase] = useState(false);
+
 
   useEffect(() => {
 
@@ -25,7 +25,7 @@ export default function Component() {
     // Get item details
     async function fetch1() {
       var res1 = await axios.get(
-        getGlobalState("backendDomain") +
+          getGlobalState("backendDomain") +
           "/api/itemListings/viewListingByID?id=" +
           id
       );
@@ -35,13 +35,14 @@ export default function Component() {
 
     async function fetch2() {
       var res2 = await axios.get(
-        getGlobalState("backendDomain") +
+          getGlobalState("backendDomain") +
           "/api/itemListings/getListingImageLinks/" +
           id
       );
       setImages(res2.data);
     }
     fetch2();
+
   }, []);
 
 
@@ -64,69 +65,78 @@ export default function Component() {
     navigate("/");
   }
 
+
   // Sets the checkout data for paypal
   function setData(){
-    setCheckOutData({...checkOutData, buyerID: user.id, sellerID: itemListing.accountId, itemListingID: itemListing.id, price: itemListing.price, currency: "AUD"});
-    console.log("Purchase form Current listing id: " + JSON.stringify(checkOutData));
     var confirm = true;
     setConfirmPurchase(confirm);
   }
 
   // axios POST for PayPal Checkout
   async function checkOut() {
-    var status = await axios.post(getGlobalState("backendDomain2") + "/api/Transactions/createPayment", checkOutData);
+    const bodyParameters = {
+      buyerID: user.id,
+      sellerID: itemListing.accountId,
+      itemListingID: itemListing.id,
+      price: itemListing.price,
+      currency: "AUD"
+    };
+
+
+    var status = await axios.post(getGlobalState("backendDomain2") + "/api/Transactions/createPayment", bodyParameters);
+    console.log("returned status " + JSON.stringify(status));
 
     // go to success or fail page
-    navigate(status);
+    navigate(status)
   }
 
   return (
-    <div className="bg-white text-black">
-      <h1 className="text-lg">{itemListing.listingTitle}</h1>
-      Condition: {itemListing.itemCondition} <br />
-      Description: {itemListing.description} <br />${itemListing.price} <br />
-      {images.map((image,index) => (
-        <div>
-          <img src={image} width="15%" height=""></img>
-        </div>
-      ))}
+      <div className="bg-white text-black">
+        <h1 className="text-lg">{itemListing.listingTitle}</h1>
+        Condition: {itemListing.itemCondition} <br />
+        Description: {itemListing.description} <br />${itemListing.price} <br />
+        {images.map((image,index) => (
+            <div>
+              <img src={image} width="15%" height=""></img>
+            </div>
+        ))}
 
 
-       {/*If: logged-in user == creator of item listing, show below*/}
-      {user.id === itemListing.accountId? (
-              <div>
-                <button className="text-yellow-400" onClick={() => navigate("/editListing?id=" + itemListing.id)}>Edit Listing</button>
-                <p className="text-orange-600">Warning! Deleting process is permanent!</p>
-                <br></br>
-                <button className="text-red-600 font-bold" onClick={() => confirmDeleteListing()}>Delete Listing</button>
+        {/*If: logged-in user == creator of item listing, show below*/}
+        {user.id === itemListing.accountId? (
+            <div>
+              <button className="text-yellow-400" onClick={() => navigate("/editListing?id=" + itemListing.id)}>Edit Listing</button>
+              <p className="text-orange-600">Warning! Deleting process is permanent!</p>
+              <br></br>
+              <button className="text-red-600 font-bold" onClick={() => confirmDeleteListing()}>Delete Listing</button>
 
-                {/* If: logged-in user == creator of item listing and confirmed to delete account */}
-                {confirmDelete === true? (
-                    <div>
-                      <button className="text-red-600 font-bold" onClick={() => deleteListing()}>CONFIRM DELETE</button>
-                    </div>
-                ): (<div className="text-yellow-500 font-bold"></div>)
-                }
-              </div>
+              {/* If: logged-in user == creator of item listing and confirmed to delete account */}
+              {confirmDelete === true? (
+                  <div>
+                    <button className="text-red-600 font-bold" onClick={() => deleteListing()}>CONFIRM DELETE</button>
+                  </div>
+              ): (<div className="text-yellow-500 font-bold"></div>)
+              }
+            </div>
 
-          ): (<div className="text-yellow-500 font-bold"></div>)
-      }
+        ): (<div className="text-yellow-500 font-bold"></div>)
+        }
 
 
-      {/*Checks that a user ID is present*/}
-      {user.id != null && user.id !== itemListing.accountId? (
-              <div>
-                <button className="text-green-400 font-bold" onClick={setData} >Purchase item</button>
-                {confirmPurchase === true? (
-                    <div>
-                      <button className="text-blue-400 font-bold" onClick={() => checkOut()}>PayPal Checkout</button>
-                    </div>
-                ): (<div className="text-yellow-500 font-bold"></div>)
-                }
-              </div>
-          ): (<div></div>)
-      }
+        {/*Checks that a user ID is present*/}
+        {user.id != null && user.id !== itemListing.accountId? (
+            <div>
+              <button className="text-green-400 font-bold" onClick={setData} >Purchase item</button>
+              {confirmPurchase === true? (
+                  <div>
+                    <button className="text-blue-400 font-bold" onClick={() => checkOut()}>PayPal Checkout</button>
+                  </div>
+              ): (<div className="text-yellow-500 font-bold"></div>)
+              }
+            </div>
+        ): (<div></div>)
+        }
 
-    </div>
+      </div>
   ); // part of return bracket
 }
