30a5d732480dc298bf70d56f96123d7cceb1bbf6
Jingxuan Feng <henryfeng24@pm.me>
2022-04-20 21:31:27 +1000

    fixed create new listing with dropdown

--- frontend/src/components/itemlisting/newlisting.jsx
+++ frontend/src/components/itemlisting/newlisting.jsx
@@ -17,6 +17,7 @@ export default function Component() {
     itemCondition: "",
     description: "",
   });
+  const [confirm, setConfirm] = useState(false);
 
   var {listingTitle, price, itemCondition, description } = formData;
   var [user, setUser] = useState({});
@@ -31,11 +32,10 @@ export default function Component() {
     {label: "BROKEN", value: "BROKEN"}
   ]
 
-  let [itemConditionDropDown, setItemConditionDropDown] = useState("Select item condition️")
+  let [itemConditionDropDown, setItemConditionDropDown] = useState("UNDEFINED")
 
   let handleItemConditionChange = (e) => {
     setItemConditionDropDown(e.target.value)
-    itemCondition = e.target.value;
   }
 
 
@@ -50,7 +50,6 @@ export default function Component() {
 
   function formInputs(event) {
     event.preventDefault();
-    // console.log(event.target.value);
     var { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   }
@@ -65,29 +64,25 @@ export default function Component() {
     setImageFile(imageFile);
 
     setImageName(
-      event.target.value
-        .toString()
-        .substring(event.target.value.toString().lastIndexOf("\\") + 1)
+        event.target.value
+            .toString()
+            .substring(event.target.value.toString().lastIndexOf("\\") + 1)
     );
 
     // setImageFile(event.target.files[0]);
   }
 
-  console.log("Form data" + JSON.stringify(formData));
 
   async function formSubmit(event) {
     event.preventDefault();
     try {
-      console.log("Form data" + JSON.stringify(formData));
+      setFormData({...formData, itemCondition: itemConditionDropDown});
 
       // Create listing
       var res1 = await axios.post(
-        getGlobalState("backendDomain") + "/api/itemListings/newItemListing/" + user.id,
-        formData);
+          getGlobalState("backendDomain") + "/api/itemListings/newItemListing/" + user.id,
+          formData);
 
-      console.log(
-        JSON.stringify("Response from posting new item listing" + res1.data)
-      );
 
       // 2nd Post to upload image to S3
       if(imageFile !== null){
@@ -113,85 +108,100 @@ export default function Component() {
     }
   }
 
+  function setConfirmPost(){
+    var confirm = true;
+    setFormData({...formData, itemCondition: itemConditionDropDown});
+    setConfirm(confirm);
+  }
+
 
   return (
-    <form
-      onSubmit={formSubmit}
-      className="bg-white text-black shadow-md rounded px-96 pt-6 pb-8 mb-4 flex flex-col"
-    >
-      <h1 className="text-3xl font-bold mb-10">Create new item listing</h1>
-      <div className="mb-4">
-        <label className="block text-grey-darker text-sm font-bold mb-2">
-          listingTitle
-        </label>
+      <form
+          onSubmit={formSubmit}
+          className="bg-white text-black shadow-md rounded px-96 pt-6 pb-8 mb-4 flex flex-col"
+      >
+        <h1 className="text-3xl font-bold mb-10">Create new item listing</h1>
+        <div className="mb-4">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            listingTitle
+          </label>
+          <input
+              value={listingTitle}
+              name="listingTitle"
+              onChange={formInputs}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
+              type="text"
+              placeholder="intresting title"
+              required
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            price
+          </label>
+          <input
+              value={price}
+              name="price"
+              onChange={formInputs}
+              className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              type="text"
+              placeholder="42"
+              required
+          />
+        </div>
+
+
+        <div className="mb-6">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            itemCondition
+          </label>
+          <select onChange={handleItemConditionChange}>
+            <option value="UNDEFINED"> -- Select your item's condition -- </option>
+            {
+              itemConditions.map((itemConditionDropDown) => <option value={itemConditionDropDown.value}>{itemConditionDropDown.label}</option>)
+            }
+          </select>
+        </div>
+
+        <div className="mb-6">
+          <label className="block text-grey-darker text-sm font-bold mb-2">
+            description
+          </label>
+          <input
+              value={description}
+              name="description"
+              onChange={formInputs}
+              className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
+              type="text"
+              placeholder="tell us about this item"
+              required
+          />
+        </div>
         <input
-          value={listingTitle}
-          name="listingTitle"
-          onChange={formInputs}
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker"
-          type="text"
-          placeholder="intresting title"
-          required
-        />
-      </div>
-      <div className="mb-6">
-        <label className="block text-grey-darker text-sm font-bold mb-2">
-          price
-        </label>
-        <input
-          value={price}
-          name="price"
-          onChange={formInputs}
-          className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
-          type="text"
-          placeholder="42"
-          required
-        />
-      </div>
+            type="file"
+            id="image-input"
+            accept="image/jpeg, image/png, image/jpg"
+            onChange={formImage}
+        ></input>
 
 
-      <div className="mb-6">
-        <label className="block text-grey-darker text-sm font-bold mb-2">
-          itemCondition
-        </label>
-        <select onChange={handleItemConditionChange}>
-          <option value="Select condition️"> -- Select your item's condition -- </option>
-          {
-            itemConditions.map((itemConditionDropDown) => <option value={itemConditionDropDown.value}>{itemConditionDropDown.label}</option>)
-          }
-        </select>
-      </div>
+        <div className="mt-5 flex items-center justify-between">
+          <button className="text-blue-400 font-bold" onClick={() => setConfirmPost()}>Confirm</button>
+        </div>
+        {confirm === true? (
+            <div className="mt-5 flex items-center justify-between">
+              <button
+                  className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
+                  type="submit"
+              >
+                Create listing
+              </button>
+            </div>
+        ): (<div></div>)
+        }
 
-      <div className="mb-6">
-        <label className="block text-grey-darker text-sm font-bold mb-2">
-          description
-        </label>
-        <input
-          value={description}
-          name="description"
-          onChange={formInputs}
-          className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3"
-          type="text"
-          placeholder="tell us about this item"
-          required
-        />
-      </div>
-      <input
-        type="file"
-        id="image-input"
-        accept="image/jpeg, image/png, image/jpg"
-        onChange={formImage}
-      ></input>
 
-      <div className="mt-5 flex items-center justify-between">
-        <button
-          className="bg-blue-500 hover:bg-blue-dark text-white font-bold py-2 px-4 rounded"
-          type="submit"
-        >
-          Create listing
-        </button>
-      </div>
-      <h1 className="mt-5 text-red-500">{error}</h1>
-    </form>
+        <h1 className="mt-5 text-red-500">{error}</h1>
+      </form>
   );
 }
